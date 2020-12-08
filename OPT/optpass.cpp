8bf3e537f43408103fafcabc728eea8e5932c8b3
Morgan Chuo Borjigin-Wang <mbwang@eecs583a.eecs.umich.edu>
2020-12-08 17:32:51 +0000

    Add opt directory and notes

--- OPT/optpass.cpp
+++ OPT/optpass.cpp
@@ -24,41 +24,129 @@ using namespace llvm;
 /*
 TODO: address potential issue that our profile data might be invalidated by other transforming passes
 running BEFORE our last pass.
-TODO: Allow running this pass with requireAnalysis<> or something (whatever it's called)
+*/
+
+/*
+    auto *SE = getAnalysisIfAvailable<ScalarEvolutionWrapperPass>();
+    MemorySSA *MSSA = EnableMSSALoopDependency
+                          ? (&getAnalysis<MemorySSAWrapperPass>().getMSSA())
+                          : nullptr;
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<BranchProbabilityInfoWrapperPass>();
+    AU.addRequired<BlockFrequencyInfoWrapperPass>();
+    AU.addPreserved<DominatorTreeWrapperPass>();
+    AU.addRequired<DominanceFrontierWrapperPass>();
+    AU.addPreserved<LoopInfoWrapperPass>();
+    AU.addRequired<TargetLibraryInfoWrapperPass>();
+    if (EnableMSSALoopDependency) {
+      AU.addRequired<MemorySSAWrapperPass>();
+      AU.addPreserved<MemorySSAWrapperPass>();
+    }
+    AU.addRequired<TargetTransformInfoWrapperPass>();
+    getLoopAnalysisUsage(AU);
+  }
+
+
+loop:
+  store(memLocB) (memLoc IS variant)
+  %k = load(memLocA) (memLocA invariant)
+  memLocA and memLocB never alias -> hoist load?
+load(memLocA)\
+
+common expression elimination
+
+%memLocA = alloca i32 // I have storage somewhere for an i32 which could be a register
+
+
+int main() {
+  Class* a = new Class(argv[1]);
+  Class* b = a;
+
+  fn(a);
+  fn(b);
+}
+
+common expression elimination?
+double calcStuff(rect* first, rect* second) {
+  return sqrt(first->area) / (sqrt(second->area) + 1);
+}
+
+arg1 = load f64 first
+val1 = call sqrt(arg1)
+arg2 = load f64 second
+val2 = call sqrt(arg2)
+val3 = add val2 1
+val4 = div val1 val3
+ret val4
+
+...
+
+arg1 = load f64 first
+val1 = call sqrt(arg1)
+val2 = val1
+if (first != second) {
+  arg2pre = load f64 second
+  val2pre = call sqrt(arg2)
+  val2 = val2pre -> store
+}
+arg2 = phi(arg1, arg2pre)
+val2 = phi(val1, val2pre)
+val3 = add val2 1
+val4 = div val1 val3
+ret val4
+
+
+%i = load(memLocA)
+%a = functionCall(%i) //
+sqrt()
+fib()
+num_digits()
+log()
+toUpper()
+
+void fcn(int og[], int copy[], int start, int end){
+  memcpy(og, copy, len);
+  if (og != copy) // do copy
+}
+int main(){
+  fcn()
+}
+
+%j = load(memLocB)
+%b = functionCall(%j)
+
+%i = load(memLocA)
+if (memLocA != memLocB) %k = load(memLocB)
+%j = phi(%i, %k)
+
+
+%hoisted = load(memLocA)
+loop:
+  %k1 = phi(%k2, %hoisted)
+  store(memLocB)
+  if (memLocB == memLocA) %i1 = load(memLocA)
+  %k2 = phi(%i1, %k1)
+  memLocA and memLocB never alias -> hoist load?
+
+  for (i = 0 -> 100) {
+    b[i] = x;
+     a = b[CONSTANT] (not alias if CONSTANT > 100)
+    a[i + 1] = y;
+  }
+*/
+
+
+/*
+PLAN:
+  - pure function optimization
+  - maybe also LICM (see if can disable register promotion)
+
+Note: can examine compiled code with
+https://stackoverflow.com/questions/10990018/how-to-generate-assembly-code-with-clang-in-intel-syntax
 */
 
 namespace {
-typedef std::pair<MemoryLocation, MemoryLocation> MemLocPair;
-
-struct hashMemLocPair {
-  size_t operator()(const MemLocPair& p) const {
-    auto hash1 = std::hash<void*>{}((void*)p.first.Ptr);
-    auto hash2 = std::hash<void*>{}((void*)p.second.Ptr);
-    return hash1 ^ hash2;
-  }
-};
-
-struct AliasStats {
-  uint32_t num_collisions;
-  uint32_t num_comparisons;
-
-  AliasStats() : num_collisions(0), num_comparisons(0) {}
-};
-struct InstLogAnalysis {
-  std::unordered_map<MemLocPair, AliasStats, hashMemLocPair> memLocPairToAliasStats;
-
-  double getAliasProbability(const MemoryLocation& loc_a, const MemoryLocation& loc_b) {
-    if (loc_a.Ptr == loc_b.Ptr) {
-      return 1.0;
-    }
-
-    auto it = InstLogAnalysis::memLocPairToAliasStats.find({loc_a, loc_b});
-    if (it == InstLogAnalysis::memLocPairToAliasStats.end()) {
-      return 0.0;
-    }
-    return (double)it->second.num_collisions / it->second.num_comparisons;
-  }
-};
 
 struct InstLogAnalysisWrapperPass : public ModulePass {
   static char ID;
@@ -173,3 +261,78 @@ static RegisterPass<InstLogAnalysisWrapperPass> X("fp_analysis", "InstLogAnalysi
                              false /* Analysis Pass */);
 
 
+ /*
+issues:
+  might detect "fake"/"useless?" aliasing across iterations/function calls
+  one fix would be to change interface to doAlias(ptrA, ptrB, %i) where i is where "start looking";
+
+loop:
+    %1: load(ptrA);      ptrA==0x1
+    %2: load(ptrB);      ptrB==0x2
+loop:
+    %1: load(ptrA);      ptrA==0x2
+    doAlias(ptrA, ptrB, i);
+    %2: load(ptrB);      ptrB==0x3
+loop:
+    %1: load(ptrA);      ptrA==0x3
+    %2: load(ptrB);      ptrB==0x4
+
+    %3: ptrC = 0x1
+
+
+ptrA = alloca
+x = alloca
+store(500, %x)
+store(600, %x)
+y = load %x
+
+int y;
+int Y;
+int* yPtr = &y;
+int* yPtr1 = &Y;
+int** yPtrPtr = &yPtr;
+*yPtrPtr = &yPtr1;
+
+
+
+%y = alloca i32, align 4
+(LOG: %y is 0xFFFFF...)
+%yPtr = alloca i32*, align 8
+(LOG: %yPtr is 0xAAAAA...)
+store i32* %y, i32** %yPtr, align 8
+%yPtr2 = alloca i32*, align 8
+
+%yPtrPtr = alloca i32**, align 8
+(LOG: %yPtrPtr is 0xBBBBB...)
+store i32** yPtr2, i32*** yPtrPtr, align 8
+%val = alloca ...
+%temp = load i32* yPtr2
+store temp, val
+
+load(ptrA);      ptrA==0x3
+load(ptrB);      ptrB==0x4
+load(ptrB);      ptrB==0x5
+load(ptrB);      ptrB==0x3
+store(ptrB)
+
+1st key is smallest ptr
+
+fn:
+    %1: load(ptrA);      ptrA==0x1
+    %2: load(ptrB);      ptrB==0x2
+    %3: store(ptrB);
+fn:
+    %1: load(ptrA);      ptrA==0x2
+    %2: load(ptrB);      ptrA==0x3
+
+  */
+ /*
+  ptrToVal
+  valToPtr
+
+  when ptr is assigned new value -> check if that value aliases with any other ptr
+
+  what have we learned?
+    memory location prob not unique to an inst?
+    need to fix our stuff
+ */
