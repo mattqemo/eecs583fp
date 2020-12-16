7073a0caf45a48c68e803a1faa47942196f012ab
Morgan Chuo Borjigin-Wang <mbwang@eecs583a.eecs.umich.edu>
2020-12-16 18:10:31 +0000

    clean up licm.c

--- 583simple/src/licm.c
+++ 583simple/src/licm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "../../fp.h"
 
 void initArr(double* A, int size) {
@@ -10,14 +11,9 @@ void initArr(double* A, int size) {
 	}
 }
 
-int iterFnPure = 1;
-
-/* this function can be made arbitrarily expensive by modifying the 3rd CL ag (iterFnPure)*/
+/* represents an arbitrary somewhat computationally expensive pure function */
 double fn_PURE_(double x) {
-	for (int i = 0; i < iterFnPure; ++i) {
-		i = i;
-	}
-	return (x * 3.1415926 + 3948.23891) / 27.5;
+	return sqrt((x * x * 3.1415926 + 3948.23891) / 27.5);
 }
 
 int main(int argc, char* argv[]) {
@@ -27,12 +23,11 @@ int main(int argc, char* argv[]) {
 	}
 	srand(time(NULL));
 	int iters, aliasPeriod;
-	sscanf(argv[1], "%d %*c %d %*c %d", &iters, &aliasPeriod, &iterFnPure);
-	fprintf(stderr, "Running %d times, with alias period %d and iterFnPure %d\n", iters, aliasPeriod, iterFnPure);
+	sscanf(argv[1], "%d %*c %d", &iters, &aliasPeriod);
+	fprintf(stderr, "Running %d times, with alias period %d\n", iters, aliasPeriod);
 
 	double A[1000];
 	initArr(A, 1000);
-	
 
 	const int j = 5;
 	int k = j + 1;
@@ -42,16 +37,8 @@ int main(int argc, char* argv[]) {
   			k = j;
 		else if(i % aliasPeriod == 1)
   			k = j + 1;
-		// A[k] = fn_PURE_(A_val);
-		fn_PURE_(*A_val_ptr);
-		A[k] = 2;
+		A[k] = fn_PURE_(*A_val_ptr);
 	}
 
 	return 0;
 }
-
-// %INITIAL = (A[j] * 3.1415926 + 3948.23891) / 27.5;
-// %temp = load(INITIAL);
-// IF ALIAS:
-//		%recalc = (A[k] * 3.1415926 + 3948.23891) / 27.5;
-// 		store(recalc, INITIAL)
